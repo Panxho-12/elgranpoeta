ae5747a8a9383986dd7ca6e078c176853cee1ee1
Panxho-12 <alvarezfran125@gmail.com>
2023-11-09 16:46:27 -0300

    termino de pruena
    xd

--- pruebadjango/libros/views.py
+++ pruebadjango/libros/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from libros.models import Libros
 from libros.forms import FormLibros
+
 
 # Create your views here.
 
@@ -23,3 +24,23 @@ def agregarLibros(request):
         return index(request)
     data = {'form' : form}
     return render(request,'libros/agregarlibros.html', data)
+
+def actualizarLibros(request, id):
+    libros = Libros.objects.get(id = id)
+    form = FormLibros(instance=libros)
+    if request.method == 'POST' :
+        form = FormLibros(request.POST, instance=libros)
+        if form.is_valid():
+            form.save()
+        return index(request)
+    data = {'form' : form}
+    return render(request, 'libros/actualizarlibros.html', data)
+
+def eliminarLibros(request, id):
+    try:
+        libros = Libros.objects.get(id=id)
+        libros.delete()
+        return redirect('libros')  
+    except Libros.DoesNotExist:
+        pass
+
--- pruebadjango/pruebadjango/urls.py
+++ pruebadjango/pruebadjango/urls.py
@@ -5,8 +5,10 @@ from libros import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', views.listadoLibros),
     path('libros/', views.listadoLibros),
-    path('agregarlibros/', views.agregarLibros)
-
+    path('agregarlibros/', views.agregarLibros),
+    path('actualizarLibros/<int:id>', views.actualizarLibros, name='actualizarLibros'),
+    path('eliminar/<int:id>/', views.eliminarLibros, name='eliminarLibros'),
+    path('libros/', views.listadoLibros, name='libros'),
 ]
