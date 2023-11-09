5059bb7cafb77634a3bd08e3d12d5095f12ff1e5
Panxho-12 <alvarezfran125@gmail.com>
2023-11-09 18:46:51 -0300

    xd

--- pruebadjango/libros/admin.py
+++ pruebadjango/libros/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Libros
 
 
-admin.site.register(Libros)
+class LibrosAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'autor', 'genero', 'disponible', 'fecha', 'cantidad']
+
+admin.site.register(Libros, LibrosAdmin)
